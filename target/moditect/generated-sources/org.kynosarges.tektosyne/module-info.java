4e568370a474726148c4af4dcc82b5b3f219235f
WarWolfen <warwolfen@gmail.com>
2020-03-25 15:22:33 +0100

    Fixed route jump behaviour.

---
+++ target/moditect/generated-sources/org.kynosarges.tektosyne/module-info.java
@@ -0,0 +1,7 @@
+
+module org.kynosarges.tektosyne {
+    exports org.kynosarges.tektosyne;
+    exports org.kynosarges.tektosyne.geometry;
+    exports org.kynosarges.tektosyne.graph;
+    exports org.kynosarges.tektosyne.subdivision;
+}
